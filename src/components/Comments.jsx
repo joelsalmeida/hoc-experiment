9e8dcb3565740b37f19420129d4c62b93df63390
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-02-08 13:51:38 -0300

    Ajusta componente Comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from 'react';
-
-export default function Comments() {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    const getComments = async () => {
-      fetch('https://jsonplaceholder.typicode.com/comments')
-        .then((response) => response.json())
-        .then((json) => setComments(json.slice(0, 10)));
-    };
-
-    getComments();
-  }, []);
-
+export default function Comments({data}) {
   return (
     <section>
       <h2>Comments</h2>
 
-      {comments.map(({ name, body, id }) => (
+      {data.map(({ name, body, id }) => (
         <div key={id}>
           <h3>{name}</h3>
           <p>{body}</p>
