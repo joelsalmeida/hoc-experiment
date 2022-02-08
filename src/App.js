ab30e24f886e4b65ad5de7905c2ae1deebfc0c4e
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-02-08 13:52:40 -0300

    Renderiza Posts e Comments via H.O. Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Comments from './components/Comments';
+import HOCList from './components/HOCList';
 import Posts from './components/Posts';
 
 function App() {
   return (
     <div className="App">
-      <Posts />
-      <Comments />
+      {HOCList(Posts, 'posts')}
+      {HOCList(Comments, 'comments')}
     </div>
   );
 }
