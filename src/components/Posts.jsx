bc3b02f3b899691a7558a56663e832f543da9254
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-02-08 13:51:54 -0300

    Ajusta componente Posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from 'react';
-
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const getPosts = async () => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => response.json())
-        .then((json) => setPosts(json.slice(0, 10)));
-    };
-
-    getPosts();
-  }, []);
-
+export default function Posts({data}) {
   return (
     <section>
       <h2>Posts</h2>
 
-      {posts.map(({ title, body, id }) => (
+      {data.map(({ title, body, id }) => (
         <div key={id}>
           <h3>{title}</h3>
           <p>{body}</p>
